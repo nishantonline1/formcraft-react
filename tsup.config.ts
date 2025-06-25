6cfab1593414c9d928bb2c806fb4475dcabb3167
Nishant Singh <nishant@nowpurchase.com>
2025-06-25 16:59:48 +0530

    Compatibility  issue

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom', 'react/jsx-runtime'],
+  esbuildOptions(options) {
+    options.jsx = 'transform'
+    options.jsxFactory = 'React.createElement'
+    options.jsxFragment = 'React.Fragment'
+  },
 }) 
